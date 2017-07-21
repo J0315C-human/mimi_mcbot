4b220e434592efb29d1f5a40032c1af608a57939
Joel Schuman <joel@cremalab.com>
2017-07-21 13:14:55 -0500

    switch to using only Twit, not node-twitterbot

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
-var Twit = require('twit');
+const Twit = require('twit');
 
-var TwitterBot = require('node-twitterbot').TwitterBot;
-var Bot = new TwitterBot({
+const T = new Twit({
  consumer_key: process.env.BOT_CONSUMER_KEY,
  consumer_secret: process.env.BOT_CONSUMER_SECRET,
  access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
 });
-var phraseArray = [ "hey twitter",
+const phraseArray = [ "hey twitter",
                     "im tweeting",
                     "tweet tweet",
                     "tweetstorm time... 1/22",
@@ -17,8 +16,14 @@ var phraseArray = [ "hey twitter",
                     "same",
                     "#dogpants go on 4 legs!!",
                     "#thedress is blue and black" ];
+
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
+
+
 var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
-Bot.tweet(phrase);
+
+T.post('statuses/update', { status: phrase }, function(err, data, response) {
+  console.log('data from callback:', data)
+})
